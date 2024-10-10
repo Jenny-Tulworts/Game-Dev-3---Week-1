354c07b069358a50f0dd0a6061cb24ebd517bfb8
Brooks <O.Brooks5757@student.leedsbeckett.ac.uk>
2024-10-10 11:56:40 +0100

    Enemy Data scriptable object created

---
+++ Game Dev 3 - Tutorial - StudentCopy/Assets/_Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData")]
+
+public class EnemyData : ScriptableObject
+{
+    public Sprite shipSprite;
+    public float shipSpeed;
+    public int shipHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
